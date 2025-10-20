ce9f33b9b2a1d5678aa7bdfde7e2a92deb503225
Mohamad Adib Tawil <mohamad.adib.tawil@gmail.com>
2025-10-20 17:20:30 +0300

    pri,ary color

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ const kOfficeArchiving = 'Office Archiving';
 
 const kFontGTSectraFine = 'GT Sectra Fine';
 
-const Color kPrimaryColor = Color(0xFF2ACF7F);
+const Color kPrimaryColor =  Color(0xFF0B2239);
 
 const Color kPOpacityColor = Color(0xffAEFFBF);
 
